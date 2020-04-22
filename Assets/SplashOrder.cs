cd27de3735e37320fe79823ff47e86270728e056
= <ivannnd701@gmail.com>
2020-04-21 22:57:46 -0700

    UI changes

---
+++ Assets/SplashOrder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SplashOrder : MonoBehaviour
+{
+    public static int scenenum;
+
+
+    void Start()
+    {
+        if (scenenum == 0) {
+            StartCoroutine(ToProductionTeamSplash());
+        }
+
+        if (scenenum == 1){
+            StartCoroutine(ToTitleScene());
+        }
+
+        
+    }
+
+    IEnumerator ToProductionTeamSplash () {
+        yield return new WaitForSeconds(5);
+        scenenum = 1;
+        SceneManager.LoadScene(1); 
+    }
+    
+    IEnumerator ToTitleScene () {
+        yield return new WaitForSeconds(5);
+        scenenum = 2;
+        SceneManager.LoadScene(2); 
+    }
+}
